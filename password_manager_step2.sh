fdfe67dd038e081b6847aedfd9c1c74c42a2005d
shingo miyabe <osakana_shingo24@i.gmobb.jp>
2024-09-29 09:18:16 +0000

    good

--- password_manager_step2.sh
+++ password_manager_step2.sh
@@ -10,8 +10,8 @@ while true; do
     case $choice in
         # Add Passwordを入力した場合
         "Add Password")
-　　　　　　# -pオプションでメッセージ（プロンプト）を表示
-　　　　　　# -p: ユーザーの入力情報を標準入力にする
+            # -pオプションでメッセージ（プロンプト）を表示
+            # -p: ユーザーの入力情報を標準入力にする
             # ユーザーからの入力を受け取り、変数に代入する
             read -p "サービス名を入力してください: " serviceName
             read -p "ユーザー名を入力してください: " userName
@@ -27,16 +27,16 @@ while true; do
         "Get Password")
             read -p "サービス名を入力してください: " serviceName
             # serviceNameに対応するpassをpasswords.txtファイルから取得
-　　　　　  # cut -d: -f3: 「:」で区切られた3番目のフィールド（パスワード）を取得
+            # cut -d: -f3: 「:」で区切られた3番目のフィールド（パスワード）を取得
             password=$(grep "^$serviceName:" passwords.txt | cut -d: -f3)
 
-　　　　　  # 文字列（password変数）の長さが空である場合に真（TURE）
+            # 文字列（password変数）の長さが空である場合に真（TURE）
             if [ -z "$password" ]; then
                 echo "そのサービスは登録されていません。"
             else
                 echo "サービス名：$serviceName"
                 # サービス名に対応するユーザー名を表示
-　　　　　　    # cut -d: -f2: 「:」で区切られた2番目のフィールド（ユーザー名）を取得。
+                # cut -d: -f2: 「:」で区切られた2番目のフィールド（ユーザー名）を取得。
                 echo "ユーザー名：$(grep "^$serviceName:" passwords.txt | cut -d: -f2)"
                 # サービス名に対応するパスワードを表示
                 echo "パスワード：$password"
